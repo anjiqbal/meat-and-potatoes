84e3342ad8775912c0c5a9aea51dfeae978e6f1a
Evangeline Amoura <evangelineamoura@yahoo.com>
2023-04-21 15:09:25 +0100

    input added, button added, handleIngredient function added, the input works

--- meat_and_potatoes/src/components/Input/index.js
+++ meat_and_potatoes/src/components/Input/index.js
@@ -1,5 +1,20 @@
 import App from "../App/App";
+import {useState} from "react";
 
 export default function Input(){
-    return
+    const [ingredient, setIngredient] = useState("");
+
+    function handleIngredient(input) {
+        setIngredient(input);
+        console.log(ingredient);
+    }
+    return (
+        <>
+        <input type ="text" onChange={(event) => {
+            handleIngredient(event.target.value)
+        }}/>
+        <button>Search</button>
+
+        </>
+    )
 }
