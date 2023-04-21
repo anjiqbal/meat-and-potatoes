9aed276109c0a95c5c077c56ad69690fbbbe20c9
Chris Branson <chrisbranson@live.co.uk>
2023-04-21 14:28:16 +0100

    Started components and checking API call

--- meat_and_potatoes/src/components/App/App.js
+++ meat_and_potatoes/src/components/App/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import Input from "../Input";
+import RecipeList from "../RecipeList"
+import {useReducer, useEffect} from "react"
 
 function App() {
-  return <main className="App"></main>;
+useEffect(() =>{
+ async function fetchdata() {
+  const res = await fetch ("www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
+  const data = await res.json()
+  console.log(data)
+} 
+fetchdata()
+} ,[]
+)
+
+  return <main className="App">
+   <h1>Meat and Potatoes</h1>
+  
+  <Input/>
+  <RecipeList/>
+
+
+
+  </main>;
 }
 
 export default App;
