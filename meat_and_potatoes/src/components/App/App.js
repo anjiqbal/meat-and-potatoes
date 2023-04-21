432690d8b52d67fd9c7e802f1fce8edf8c87223f
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-04-21 14:39:34 +0100

    added https to api link

--- meat_and_potatoes/src/components/App/App.js
+++ meat_and_potatoes/src/components/App/App.js
@@ -1,28 +1,28 @@
 import "./App.css";
 import Input from "../Input";
-import RecipeList from "../RecipeList"
-import {useReducer, useEffect} from "react"
+import RecipeList from "../RecipeList";
+import { useReducer, useEffect } from "react";
 
 function App() {
-useEffect(() =>{
- async function fetchdata() {
-  const res = await fetch ("www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast")
-  const data = await res.json()
-  console.log(data)
-} 
-fetchdata()
-} ,[]
-)
+  useEffect(() => {
+    async function fetchdata() {
+      const res = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast"
+      );
+      const data = await res.json();
+      console.log({ data });
+    }
+    fetchdata();
+  }, []);
 
-  return <main className="App">
-   <h1>Meat and Potatoes</h1>
-  
-  <Input/>
-  <RecipeList/>
+  return (
+    <main className="App">
+      <h1>Meat and Potatoes</h1>
 
-
-
-  </main>;
+      <Input />
+      <RecipeList />
+    </main>
+  );
 }
 
 export default App;
